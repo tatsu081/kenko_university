42496190602cd80f913efa0afc93480e536a8a32
tatsu081 <kassi081dm@gmail.com>
2021-04-16 14:23:28 +0900

    初期設定
    1.scss作成
    2.MicroCMS導入

---
+++ pages/_slug.vue
@@ -0,0 +1,28 @@
+<template>
+  <main class="main">
+    <h1 class="title">{{ title }}</h1>
+    <div v-html="detail"></div>
+    <router-link to="/">記事一覧に戻る</router-link>
+  </main>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  async asyncData({ params }) {
+    const { data } = await axios.get(
+      `https://kenko-university.microcms.io/api/v1/blog/${params.slug}`,
+      {
+        headers: { 'X-API-KEY': 'b683b6b8-99a7-4f01-974c-aab6ccc316f1' }
+      }
+    )
+    return data
+  },
+  data() {
+    return{
+      detail: '　{{ content.detail }}　'
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <ul>
+    <li v-for="content in contents" :key="content.id">
+      <nuxt-link :to="`/${content.id}`">
+<!--        <img :src="content.image.url" alt="">-->
+        {{ content.title }}
+      </nuxt-link>
+    </li>
+  </ul>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  async asyncData() {
+    const { data } = await axios.get(
+      // your-service-id部分は自分のサービスidに置き換えてください
+      'https://kenko-university.microcms.io/api/v1/blog',
+      {
+        // your-api-key部分は自分のapi-keyに置き換えてください
+        headers: { 'X-API-KEY': 'b683b6b8-99a7-4f01-974c-aab6ccc316f1' }
+      }
+  )
+    console.log(data)
+    return data
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
