d2d74459e7951cb39f6d596de376f43fa2843823
tatsu081 <kassi081dm@gmail.com>
2021-04-17 08:23:47 +0900

    新規登録画面　作成

---
+++ plugins/firebase.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase'
+
+const config = {
+  apiKey: "AIzaSyB0KgyPSy9XMqwGCJ-_0fM4I72HbAE1i_E",
+  authDomain: "kenko-university.firebaseapp.com",
+  projectId: "kenko-university",
+  storageBucket: "kenko-university.appspot.com",
+  messagingSenderId: "458079551199",
+  appId: "1:458079551199:web:23644809ee23d5d35a736a",
+  measurementId: "G-ZB1CGWD9WN"
+}
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(config)
+  firebase.analytics()
+}
+
+export const auth = firebase.auth
+export default firebase
