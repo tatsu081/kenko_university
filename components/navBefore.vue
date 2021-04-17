35de330aa31188614ac3aa590068c0cbe00cc416
tatsu081 <kassi081dm@gmail.com>
2021-04-17 13:18:55 +0900

    API_KEYを入力

--- components/navBefore.vue
+++ components/navBefore.vue
@@ -1,14 +1,17 @@
 <template>
   <div>
     <router-link to="/login">ログイン</router-link>
+    <router-link to="/register">新規登録</router-link>
   </div>
 </template>
 
 <script>
 import login from "../pages/login";
+import register from "../pages/register";
 export default {
   components:{
-    login
+    login,
+    register
   }
 }
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -13,12 +13,13 @@
 import axios from 'axios'
 export default {
   async asyncData() {
+    console.log(process.env.API_KEY)
     const { data } = await axios.get(
       // your-service-id部分は自分のサービスidに置き換えてください
       'https://kenko-university.microcms.io/api/v1/blog',
       {
         // your-api-key部分は自分のapi-keyに置き換えてください
-        headers: { 'X-API-KEY': 'b683b6b8-99a7-4f01-974c-aab6ccc316f1' }
+        headers: { 'X-API-KEY': process.env.API_KEY }
       }
   )
     console.log(data)
