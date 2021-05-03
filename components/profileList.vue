5ad8773434e9e3d3915f616d9514a2522807da8b
tatsu081 <kassi081dm@gmail.com>
2021-05-03 17:18:22 +0900

    blogLayout ブログカード作成
    1.記事のサイドバーと記事カード作成

--- components/profileList.vue
+++ components/profileList.vue
@@ -1,85 +1,61 @@
 <template>
-  <v-card>
+  <v-card
+    style="margin-top: 30px"
+  >
     <v-list>
-      <v-list-item link to="/category/">
+      <v-list-item to="/profile/">
         <v-list-item-content>
           <v-list-item-title class="title categoryList" style="text-align: center">
-            <p>Category</p>
+            <p>Profile</p>
           </v-list-item-title>
         </v-list-item-content>
       </v-list-item>
     </v-list>
-    <v-list nav>
-      <v-list-item-group :color="color">
-        <div v-for="(item, i) in categories" :key="i">
-          <v-divider />
-          <v-list-item :to="'/category/' + item.id">
-            <v-list-item-icon>
-              <v-icon :color="color" v-text="item.icon" style="margin-left: 20px"></v-icon>
-            </v-list-item-icon>
-
-            <v-list-item-content>
-              <v-list-item-title v-text="item.title"></v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
+    <v-card-text>
+      <p style="margin: 0 15px 20px 15px">
+        超健康オタクの僕が皆さんに、超健康になって欲しいとの思いで作成しました。
+        自分の知識や今までの健康に対しての行動がみなさまの今後のハッピーに繋がれば幸いです
+        <br />一見共通点のない2人が、それぞれの考え方や活動のことを投稿します。
+      </p>
+    </v-card-text>
+    <v-divider />
+    <v-row no-gutters>
+      <v-col cols="4">
+<!--        <v-img :src="node" aspect-ratio="1"></v-img>-->
+      </v-col>
+      <v-col cols="8" class="node-profile">
+        <div style="margin: 10px 10px 0 15px">
+          <p>ふってぃ</p>
+          <span>Webエンジニア　理学療法士</span>
+          <br />
+          <v-btn
+            color="#00acee"
+            href="https://twitter.com/node_mental"
+            target="_blank"
+            rel="noopener noreferrer"
+            icon
+          >
+            <v-icon>mdi-twitter</v-icon>
+          </v-btn>
         </div>
-      </v-list-item-group>
-    </v-list>
+      </v-col>
+    </v-row>
+    <v-divider />
+
+    <v-layout row wrap>
+      <v-spacer />
+      <v-btn text large to="/profile/" style="margin-right: 15px; color: #54AD81;">プロフィール詳細 ＞</v-btn>
+    </v-layout>
   </v-card>
 </template>
 
 <script>
+
 export default {
-  props: {
-    /*categories: {
-            type: Array,
-            default: []
-        },*/
-    color: {
-      type: String,
-      default: "#54AD81"
-    }
-  },
-  data() {
-    return {
-      categories: [
-        {
-          title: "食事",
-          id: "meal",
-          icon: "mdi-food-fork-drink"
-        },
-        {
-          title: "運動",
-          id: "exercise",
-          icon: "mdi-arm-flex"
-        },
-        {
-          title: "睡眠",
-          id: "sleep",
-          icon: "mdi-sleep"
-        },
-        {
-          title: "禁欲",
-          id: "abstinence",
-          icon: "mdi-corn-off"
-        },
-        {
-          title: "マインド",
-          id: "mind",
-          icon: "mdi-thought-bubble-outline"
-        },
-        {
-          title: "その他",
-          id: "others",
-          icon: "mdi-dots-vertical-circle-outline"
-        },
-      ]
-    };
-  }
-}
+};
 </script>
 
-<style scoped>
+<style lang="scss">
 .categoryList p {
   position: relative;
   display: inline-block;
@@ -90,12 +66,32 @@ export default {
   position: absolute;
   bottom: -15px;
   display: inline-block;
-  width: 100px;
+  width: 60px;
   height: 5px;
   left: 50%;
   -webkit-transform: translateX(-50%);
   transform: translateX(-50%);
   background-color: #54AD81;
   border-radius: 2px;
+}
+.node-profile p {
+  color: #54AD81;
+  position: relative;
+  margin: 5px 0 0 0;
+  border-bottom: solid 1px #c8e4d3;
+  position: relative;
+}
+
+.node-profile p:after {
+  position: absolute;
+  content: " ";
+  display: block;
+  border-bottom: solid 1px #54AD81;
+  bottom: -1px;
+  width: 20%;
+}
+
+.node-profile span {
+  font-size: 9pt;
 }
 </style>
--- pages/category/_category/index.vue
+++ pages/category/_category/index.vue
@@ -1,26 +1,37 @@
 <template>
   <div>
     <div class="twoColumn__container">
-      <ul class="blog__main__container">
-        <li v-for="content in contents" :key="content.id">
-          <nuxt-link :to="`/${content.id}`">
-            <img :src="content.image.url" alt="">
-            <p>{{ content.title }}</p>
-            <p>{{ content.category && content.category.name }}</p>
+      <ul class="blog__container"　style="padding:0;">
+        <li
+          v-for="content in contents" :key="content.id" class="blog__box">
+          <nuxt-link :to="`/${content.id}`" class="blog__inner">
+            <div class="blog__left">
+              <img :src="content.image.url" alt="健康大学">
+            </div>
+            <div class="blog__right">
+              <p>
+                <span class="blog__right__category">{{ content.category && content.category.name }}</span>
+                <span class="blog__right__date">作成日 : {{formatDate(content.createdAt)}}</span>
+              </p>
+              <h2 class="blog__right__title">{{ content.title }}</h2>
+              <p></p>
+            </div>
           </nuxt-link>
         </li>
+        <v-layout v-if="length > 12" row wrap justify-end>
+          <v-btn
+            :to="'/category/' + this.$route.params.category + '/page/2'"
+            color="secondary"
+            large
+            outlined
+          >次ページ ＞</v-btn>
+        </v-layout>
       </ul>
+      <div class="sidebar__container">
       <sidebar/>
+      </div>
     </div>
 
-    <v-layout v-if="length > 12" row wrap justify-end class="ma-7">
-      <v-btn
-        :to="'/category/' + this.$route.params.category + '/page/2'"
-        color="secondary"
-        large
-        outlined
-      >次ページ ＞</v-btn>
-    </v-layout>
   </div>
 </template>
 
@@ -50,6 +61,15 @@ export default {
   },
   components:{
     sidebar
+  },
+  methods: {
+    formatDate(iso) {
+      const date = new Date(iso);
+      const yyyy = new String(date.getFullYear());
+      const mm = new String(date.getMonth() + 1).padStart(2, "0");
+      const dd = new String(date.getDate()).padStart(2, "0");
+      return `${yyyy}-${mm}-${dd}`;
+    }
   }
 }
 </script>
