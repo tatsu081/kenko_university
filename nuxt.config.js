5f50114f2764fde15c18cc376a294e33ca7bdd17
tatsu081 <kassi081dm@gmail.com>
2021-05-23 14:48:37 +0900

    middlewearAuth　ミドルウェア認証
    1.リロード時のステイト情報取得

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@ const { API_KEY } = process.env;
 
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
-  mode: 'universal',
+  ssr: false,
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
